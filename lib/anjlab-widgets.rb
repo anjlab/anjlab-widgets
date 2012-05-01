be64acc4ae5193a6d51e7041dc85f07d00e59a29
Yury Korolev <yury.korolev@gmail.com>
2012-05-01 15:56:47 +0400

    date time parse helpers

--- lib/anjlab-widgets.rb
+++ lib/anjlab-widgets.rb
@@ -3,5 +3,39 @@ require "anjlab-widgets/engine"
 
 module Anjlab
   module Widgets
+    def self.date_format 
+      case ::I18n.default_locale
+      when :ru
+        "%d.%m.%Y"
+      when :en
+        "%m/%d/%Y"
+      else
+        "%Y-%m-%d"
+      end
+
+    end
+
+    def self.parse_date date_string
+      return nil if date_string.blank?
+      # native controls date format
+      if date_string =~ /\d{4}-[01]\d-[0123]\d/
+        ::Date.parse date_string, "%Y-%m-%d"
+      else
+        ::Date.parse date_string, date_format
+      end
+    end
+
+    def self.parse_time time_string
+      return nil if date_string.blank?
+      now = Time.now
+      parts = time_string.split(':')
+      Time.local(now.year, now.month, day, parts[0], parts[1])
+    end
+
+    def self.parse_date_time date_string, time_string
+      date = parse_date date_string
+      time = parse_time time_string
+      Time.local(date.year, date.month, date.day, time.hour, time.min)
+    end
   end
 end
